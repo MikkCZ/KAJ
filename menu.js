9d5aef90077ae2a65341252a00a087abb5e8ef3b
Michal Stanke <michal@stanke.cz>
2016-06-11 08:31:39 +0200

    Indicate when running from local filesystem

--- menu.js
+++ menu.js
@@ -56,4 +56,10 @@ Initialize menu object after document is loaded
 */
 document.addEventListener("DOMContentLoaded", function(event) {
     var menu = new Menu();
+    if (window.location.protocol == "file:") {
+        var runningLocal = document.createElement("span");
+        runningLocal.id = "local";
+        runningLocal.innerHTML = "local";
+        document.querySelector("header").appendChild(runningLocal);
+    }
 });
